28505eb0c6a027593b8610e2c0e16607dc553b31
Swarzstein <zorrillagja@gmail.com>
2023-01-04 15:56:23 -0400

    Display list of items completed

---
+++ modules/datadex.js
@@ -0,0 +1,13 @@
+const getPokemonList = async () => {
+  const request = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
+  const response = await request.json();
+  return response.results;
+};
+const getPokemon = async (url) => {
+  const request = await fetch(url);
+  const response = await request.json();
+  //console.log(response);
+  return response;
+};
+
+export { getPokemonList, getPokemon };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './index.css';
+import { displayPokemonList, pokemonList } from '../modules/display.js';
+
+displayPokemonList();
+/*const displayModal = (n) {
+    pokemonList[n]
+};*/
