37ad68053b2abb649c9f8f6970b4ba63ae1903a3
Swarzstein <zorrillagja@gmail.com>
2023-01-06 14:29:32 -0400

    itemCounter test working

---
+++ __test__/cardsCounter.test.js
@@ -0,0 +1,35 @@
+/*
+ * @jest-environment jsdom
+ */
+
+import counter from '../modules/cardsCounter.js';
+
+describe('Count items', () => {
+  test('Counting 4 cards', () => {
+    let cards = '';
+    for (let i = 0; i < 4; i += 1) {
+      cards += '<div class="card"></div>';
+    }
+    document.body.innerHTML = cards;
+    const cardsList = counter();
+    expect(cardsList).toEqual(4);
+  });
+  test('Counting 10 cards', () => {
+    let cards = '';
+    for (let i = 0; i < 10; i += 1) {
+      cards += '<div class="card"></div>';
+    }
+    document.body.innerHTML = cards;
+    const cardsList = counter();
+    expect(cardsList).toEqual(10);
+  });
+  test('Counting 20 cards', () => {
+    let cards = '';
+    for (let i = 0; i < 20; i += 1) {
+      cards += '<div class="card"></div>';
+    }
+    document.body.innerHTML = cards;
+    const cardsList = counter();
+    expect(cardsList).toEqual(20);
+  });
+});
---
+++ modules/cardsCounter.js
@@ -0,0 +1,7 @@
+let count = 0;
+
+count = 20;
+
+const counter = () => count;
+
+export default counter;
